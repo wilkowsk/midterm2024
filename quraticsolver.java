fc811fcca014589ef9c5c0420ca505efe7de3033
Jamie Wilkowski <wilkowsk@umich.edu>
2024-03-07 14:32:48 -0500

    fixed typo

--- quraticsolver.java
+++ quraticsolver.java
@@ -4,7 +4,7 @@ public class quraticsolver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a = 1;
-		int b = 0;
+		int b = 1;
 		int c = 0;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
